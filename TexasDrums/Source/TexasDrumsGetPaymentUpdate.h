87242f66d1a528f0ddbf6a6284d651bbabd2c386
Corey Roberts <corey.roberts@mapmyfitness.com>
2012-07-29 22:57:09 -0500

    Updated PaymentUpdate controller.

---
+++ TexasDrums/Source/TexasDrumsGetPaymentUpdate.h
@@ -0,0 +1,13 @@
+//
+//  TexasDrumsGetPaymentUpdate.h
+//  TexasDrums
+//
+//  Created by Corey Roberts on 7/29/12.
+//  Copyright (c) 2012 Corey Roberts. All rights reserved.
+//
+
+#import "TexasDrumsRequest.h"
+
+@interface TexasDrumsGetPaymentUpdate : TexasDrumsRequest
+
+@end
---
+++ TexasDrums/Source/TexasDrumsGetPaymentUpdate.m
@@ -0,0 +1,13 @@
+//
+//  TexasDrumsGetPaymentUpdate.m
+//  TexasDrums
+//
+//  Created by Corey Roberts on 7/29/12.
+//  Copyright (c) 2012 Corey Roberts. All rights reserved.
+//
+
+#import "TexasDrumsGetPaymentUpdate.h"
+
+@implementation TexasDrumsGetPaymentUpdate
+
+@end
